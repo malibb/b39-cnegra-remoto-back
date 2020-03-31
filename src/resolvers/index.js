514dcab9ded3761eaa6691b0e9967b06521bd217
Malinali <mali@devf.mx>
2020-03-30 20:01:36 -0600

    dd graphql-scalars

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,7 +1,9 @@
 const AuthorResolver = require('./AuthorResolvers');
 const PostsResolver = require('./PostResolvers');
-
+const {EmailAddressResolver, URLResolver} = require('graphql-scalars');
 module.exports = {
+    EmailAdd: EmailAddressResolver,
+    URL: URLResolver,
     Query: {
         ...AuthorResolver.Query,
         ...PostsResolver.Query,
