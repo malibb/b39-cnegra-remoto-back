16265bff5fff96a14cbd5c7b29f0c553f0f02de1
Malinali <mali@devf.mx>
2020-03-30 22:40:22 -0600

    login

--- src/resolvers/AuthorResolvers/Mutation.js
+++ src/resolvers/AuthorResolvers/Mutation.js
@@ -1,5 +1,5 @@
 const { createOneAuthor, updateById, deleteOneAuthor }= require('../../services/AuthorService');
-
+const authenticate = require('../../utils/authenticate');
 // (root, params, context, info)
 const createAuthor = async (_, { data }) => {
     const author = await createOneAuthor(data); // Servicio de base de datos
@@ -17,8 +17,20 @@ const deleteAuthor = async (_, { id }) => {
     return 'Author delete';
 };
 
+const login = async (_, params)=>{
+    const token = authenticate(params)
+        .catch(e => { throw e;});
+    return {
+        token: token,
+        message: 'Login Sucessful'
+    };
+};
+
+// Authorization
+
 module.exports = {
     createAuthor,
     updateAuthor,
     deleteAuthor,
+    login,
 };
