aaab5a706604b57e8d8374c56b4bfa99025b2e5a
Malinali <mali@devf.mx>
2020-03-31 21:52:07 -0600

    add configuration to storage files

--- src/resolvers/PostResolvers/Mutation.js
+++ src/resolvers/PostResolvers/Mutation.js
@@ -1,6 +1,19 @@
 const { createOnePost, updateOnePost, deleteOnePost, getOnePost } = require('../../services/PostService');
+const storage = require('../../utils/storage');
 
 const createPost = async (_, {data}, {userAuth}) => {
+    // data.cover = 'data:img;dwe23e2'; https://cloudinary.com/dseffr
+    // console.log(data.cover);
+    if (data.cover) {
+        const { createReadStream } = await data.cover;
+        const stream = createReadStream();
+        const storageInfo = await storage({stream});
+        console.log(storageInfo);
+        data = {
+            ...data,
+            cover: storageInfo.url, //url image
+        };
+    }
     const post = await createOnePost(data);
     if(post) {
         userAuth.posts.push(post._id);
@@ -12,6 +25,16 @@ const createPost = async (_, {data}, {userAuth}) => {
 };
 
 const updatePost = async (_, {id, data}) => {
+    if (data.cover) {
+        const { createReadStream } = await data.cover;
+        const stream = createReadStream();
+        const storageInfo = await storage({stream});
+        console.log(storageInfo);
+        data = {
+            ...data,
+            cover: storageInfo.url, //url image
+        };
+    }
     const post = await updateOnePost(id, data);
     return post;
 };
